b785d67cf5b82e0715032c341525f1211f77face
Florian <Florian@Florian-ACER>
2014-06-16 15:54:56 +0200

    Die Wurzel alles Bösen...

--- 4FarbenProblem/src/dhbw/horb/projekte/programmieren/farbenproblem/Feld.java
+++ 4FarbenProblem/src/dhbw/horb/projekte/programmieren/farbenproblem/Feld.java
@@ -1,5 +1,13 @@
 package dhbw.horb.projekte.programmieren.farbenproblem;
 
 public class Feld {
+	private int id;
 
+	public Feld(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
 }
