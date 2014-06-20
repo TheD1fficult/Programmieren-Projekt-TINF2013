a428748d49674f758e9182de2f2540792a661cad
TheD1fficult <i13014@hb.dhbw-stuttgart.de>
2014-06-20 10:50:10 +0200

    GUI update

--- 4FarbenProblem/src/dhbw/horb/projekte/programmieren/farbenproblem/gui/Ober.java
+++ 4FarbenProblem/src/dhbw/horb/projekte/programmieren/farbenproblem/gui/Ober.java
@@ -1,32 +1,30 @@
 package dhbw.horb.projekte.programmieren.farbenproblem.gui;
 
-import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JToolBar;
+
 import java.awt.BorderLayout;
-import javax.swing.JComboBox;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JPopupMenu;
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import javax.swing.JButton;
-import javax.swing.JMenu;
-import java.awt.Panel;
-import java.awt.FlowLayout;
-import javax.swing.BoxLayout;
-import java.awt.Button;
-import javax.swing.JPanel;
-import java.awt.CardLayout;
+
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.Box;
-import java.awt.Color;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 
 public class Ober {
+	public String inputString=null;
 
 	private JFrame frmfarbenproblem;
 
@@ -61,33 +59,38 @@ public class Ober {
 		frmfarbenproblem.setTitle("4Farben-Problem");
 		frmfarbenproblem.setBounds(100, 100, 450, 300);
 		frmfarbenproblem.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		frmfarbenproblem.setJMenuBar(menuBar);
-		
+
 		JMenu mnDatei = new JMenu("Datei");
 		menuBar.add(mnDatei);
-		
+
 		JMenuItem mntmNeuesFeld = new JMenuItem("Neues Feld");
 		mnDatei.add(mntmNeuesFeld);
-		
+		mntmNeuesFeld.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				inputString = JOptionPane.showInputDialog(null, "Feldgröße (Default 4)",
+						"Neues Feld", 1);
+				
+				int[] x={10,200,200,10};
+				int[] y={10,10,200,200};
+				frmfarbenproblem.getContentPane().add(new MyField(x,y));
+				frmfarbenproblem.setVisible(true);
+			}
+		});
+
 		JMenuItem mntmBeenden = new JMenuItem("Beenden");
 		mnDatei.add(mntmBeenden);
-		
-		JPanel panel = new JPanel();
-		frmfarbenproblem.getContentPane().add(panel, BorderLayout.CENTER);
-		GroupLayout gl_panel = new GroupLayout(panel);
-		gl_panel.setHorizontalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 434, Short.MAX_VALUE)
-		);
-		gl_panel.setVerticalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 241, Short.MAX_VALUE)
-		);
-		panel.setLayout(gl_panel);
+		mntmBeenden.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
 	}
-
+	
 	private static void addPopup(Component component, final JPopupMenu popup) {
 		component.addMouseListener(new MouseAdapter() {
 			public void mousePressed(MouseEvent e) {
@@ -95,14 +98,40 @@ public class Ober {
 					showMenu(e);
 				}
 			}
+
 			public void mouseReleased(MouseEvent e) {
 				if (e.isPopupTrigger()) {
 					showMenu(e);
 				}
 			}
+
 			private void showMenu(MouseEvent e) {
 				popup.show(e.getComponent(), e.getX(), e.getY());
 			}
 		});
+		
 	}
+
 }
+
+class MyField extends JComponent {
+	private int[] x;
+	private int[] y;
+	private int[] x1;
+	private int[] y1;
+	public MyField(int[] x,int[] y){
+	    this.x = x;
+	    this.y = y;
+	    this.x1 = x;
+	    this.y1 = y;
+		
+	}
+	  public void paint(Graphics g) {
+	
+	    g.setColor(Color.yellow);
+	    g.fillPolygon (x, y, x.length);  
+
+	    g.setColor(Color.black);
+	    g.drawPolygon (x1, y1, x1.length); 
+	  }
+	}
